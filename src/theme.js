2b668ac88fe7003c96d88ea11df786460125ec36
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-01-15 14:55:21 +0000

    Added custom theme

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,8 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import { lightGreen, orange, red, yellow } from '@material-ui/core/colors';
 
 // Create a theme instance.
-const theme = createMuiTheme({
+const defaultTheme = createMuiTheme({
   palette: {
     primary: {
       main: '#556cd6',
@@ -19,4 +19,16 @@ const theme = createMuiTheme({
   },
 });
 
-export default theme;
+const plantTreesTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: lightGreen[800],
+    },
+    secondary: {
+      main: orange[900],
+    },
+  },
+});
+
+export default plantTreesTheme;
+export { defaultTheme };

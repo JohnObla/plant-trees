a94e8bc8de039958766ca6b28fb61278b5f13ade
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-01-15 22:51:24 +0000

    Added subtext below hero image

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,9 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   subtext: {
-    color: theme.palette.secondary,
+    backgroundColor: theme.palette.secondary.main,
+    color: theme.palette.common.white,
+    // padding: theme.spacing(1),
   },
 }));
 
@@ -24,42 +26,19 @@ export default function Index() {
   return (
     <Box>
       <HeroImage />
-      {/* <CardContent>
-        <Typography variant="body2" component="p">
-          In 2020, over 50 million trees were cut down
-        </Typography>
-      </CardContent> */}
 
-      <Box className={classes.subtext} maxWidth="xs">
-        asfasf
+      <Box
+        className={classes.subtext}
+        px={{ xs: 3, sm: 4, md: 5, lg: 6 }}
+        py={{ xs: 2, sm: 3 }}
+      >
+        <Typography>
+          Over 20 million trees have been cut down in aid of tree spaces. We
+          need your help now more than ever. Please help us restore them.
+        </Typography>
       </Box>
 
-      <Typography>
-        Trees are dying everyday from lack of nutrition. This is from earlier
-        this year. The headline reads: “Nation Shudders At Large Block Of
-        Uninterrupted Text.” “Unable to rest their eyes on a colorful photograph
-        or boldface heading that could be easily skimmed and forgotten,
-        Americans collectively recoiled Monday when confronted with a solid
-        block of uninterrupted text. “Dumbfounded citizens from Maine to
-        California gazed helplessly at the frightening chunk of print, unsure of
-        what to do next. “Without an illustration, chart, or embedded YouTube
-        video to ease them in, millions were frozen in place, terrified by the
-        sight of one long, unbroken string of English words. ” ‘It demands so
-        much of my time and concentration,’ said Chicago resident Dale Huza, who
-        was confronted by the confusing mound of words early Monday afternoon.
-        ‘This large block of text, it expects me to figure everything out on my
-        own, and I hate it.’ ” This is a sign of how rough the outlook sometimes
-        seems for our culture of reading and writing. In fact, every generation
-        fears the death of literacy at the hands of some new media technology.
-        And yet I’m here to share some optimism. After long existence as a
-        confirmed cynic who shared the general belief in our imminent cultural
-        doom, I felt an unfamiliar sensation 15 years ago when the Internet came
-        over the horizon: I found myself becoming excited and hopeful. When I
-        looked at the Internet I saw a medium that involves a huge amount of
-        reading. Sure, a lot of it is presented in a highly decorated or
-        distracting form. But a lot of it is in large blocks of uninterrupted
-        text, too!
-      </Typography>
+      <Box px="2"></Box>
       <ProTip />
       <Copyright />
     </Box>

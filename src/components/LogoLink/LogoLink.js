68db1fffc4ba90992dc91933226ce6eca39a9c5c
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-01-16 17:08:44 +0000

    Changed color to white through useStyles

--- src/components/LogoLink/LogoLink.js
+++ src/components/LogoLink/LogoLink.js
@@ -2,10 +2,19 @@ import React from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import EcoIcon from '@material-ui/icons/Eco';
 import Link from '../../Link';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  whiteColor: {
+    color: 'white',
+  },
+}));
 
 const LogoLink = props => {
+  const classes = useStyles();
   return (
     <IconButton
+      className={classes.whiteColor}
       color="inherit"
       edge="start"
       aria-label="home page"

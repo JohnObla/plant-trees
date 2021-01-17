536b682fa175589bbc2e3df1bbcd545f7d97e4ac
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-01-17 22:29:06 +0000

    Hero image fills screen width

--- src/components/HeroImage/HeroImage.js
+++ src/components/HeroImage/HeroImage.js
@@ -9,7 +9,6 @@ const useStyles = makeStyles(theme => ({
   root: {
     backgroundImage:
       'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("/clearing-overhead-trees.jpg")',
-    //   'url("/clearing-overhead-trees.jpg")',
     height: '300px',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
@@ -30,19 +29,7 @@ const useStyles = makeStyles(theme => ({
 const HeroImage = () => {
   const classes = useStyles();
   return (
-    // <Image
-    //   className={classes.root}
-    //   src="/clearing-overhead-trees.jpg"
-    //   alt="overhead camera shot of trees in a forest"
-    //   layout="fixed"
-    //   height="200"
-    //   width="200"
-    // />
-
-    <Box className={classes.root}>
-      {/* <Typography className={classes.heading} variant="h4" component="h1">
-        The trees need you.
-      </Typography> */}
+    <Box maxWidth="100%" className={classes.root}>
       <Typography className={classes.heading} variant="h1" component="h1">
         The trees need you.
       </Typography>
